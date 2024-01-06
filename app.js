a538a5366436708e8d908566fc62e9593042d450
Marcela Ramirez <mymarce27@gmail.com>
2024-01-06 13:03:50 -0500

    commit vacio

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const app = express();
+const path = require('path');
 const methodOverride = require('method-override');
 
 //Routes-Imports
@@ -11,12 +12,16 @@ const authRoutes = require('./src/routes/auth.routes');
 
 const PORT = 4001;
 
-
-app.use(express.static('public'));
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, './src/views'));
 
 app.use(express.urlencoded());
 app.use(express.json());
 app.use(methodOverride('_method'));
+
+
+app.use(express.static('public'));
+
 
 app.use('/', mainRoutes);
 app.use('/shop', shopRoutes);
